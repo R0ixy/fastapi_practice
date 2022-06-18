d9c4184342d6381643123703bbbfa6847d1a1f31
R0ixy <nikita6kom@gmail.com>
2022-06-18 15:54:10 +0300

    added user validation

--- schemas/user.py
+++ schemas/user.py
@@ -1,20 +1,41 @@
+from uuid import UUID
+
 from pydantic import BaseModel
+from pydantic import constr
 
 
-class User(BaseModel):
-    user_id: str
+class UserBase(BaseModel):
+    """
+    Base user class
+    """
     firstName: str
     lastName: str
-    email: str
-    phoneNumber: int
+    email: constr(
+        regex=r'^[\w.+\-]+@gmail\.com$'
+    )
+    phoneNumber: constr(
+        strip_whitespace=True,
+        regex=r'^\+?3?8?(0[5-9][0-9]\d{7})$'
+    )
+    # TODO fix phone regex to work with all numbers (Now working only with Ukrainian)
+    # TODO fix email regex to work all email domains (Now working only with gmail)
+
+
+class UserCreate(UserBase):
+    """
+    User class with all fields for new entry creation
+    """
+    password: str
+
+
+class User(UserBase):
+    """
+    User class for database interaction
+    """
+    user_id: int
+    user_uuid: UUID
     hashed_password: str
-    is_active: bool = 0
+    is_active: bool
 
     class Config:
         orm_mode = True
-
-
-class CreateUser(User):
-    password: str
-
-# TODO additional inheritance for is_active field
